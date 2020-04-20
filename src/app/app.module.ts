10c91794ba6aedcdddb195ea479de6f2fdf6987f
rajatchauhan126 <rajatchauhan126@gmail.com>
2020-04-21 00:59:14 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { AppRoutingModule } from "./app-routing.module";
@@ -23,7 +22,6 @@ import { ShoppingListService } from "./shopping-list/shopping-list.service";
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeStartComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
