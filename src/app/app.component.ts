b875718a471cf693f8d22b7048ea1974cc140614
rajatchauhan126 <rajatchauhan126@gmail.com>
2020-04-20 14:46:41 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  feature : string = 'recipes';
+  onSelectFeature(feature: string) {
+    this.feature = feature;
+  }
 }
